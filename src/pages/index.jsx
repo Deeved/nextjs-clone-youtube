52d675863b424b1911b46d5563fdf37ddfd53aed
Deeved Hiuston <deeved@gmail.com>
2021-03-03 21:47:46 -0300

    Material-ui configuration

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+export default function Home() {
+  return (
+    <div>
+      clone youtube com Nextjs e material-ui
+      <Button variant="contained" color="secondary">Default</Button>
+    </div>
+  );
+}
